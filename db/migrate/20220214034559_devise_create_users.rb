5d0d0e77e7ff0eb1a96f4fbb39787f31a7698642
key3966 <ev0.kpx@gmail.com>
2022-02-14 15:24:33 +0900

    usersテーブル作成

--- db/migrate/20220214034559_devise_create_users.rb
+++ db/migrate/20220214034559_devise_create_users.rb
@@ -6,6 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false, unique: true
+      t.string :sex,                null: false
+      t.date   :birthday,           null: false
+      t.references :fridge,         null: true, foreign_key: true
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -18,4 +18,22 @@ ActiveRecord::Schema.define(version: 2022_02_14_040634) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
+    t.string "sex", null: false
+    t.date "birthday", null: false
+    t.bigint "fridge_id"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["fridge_id"], name: "index_users_on_fridge_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "users", "fridges"
 end
