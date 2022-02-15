9e17ac8986a20d5ff264c71bb1ff087d139ba207
key3966 <ev0.kpx@gmail.com>
2022-02-15 12:09:32 +0900

    fridgesコントローラー編集

--- app/controllers/fridges_controller.rb
+++ app/controllers/fridges_controller.rb
@@ -1,4 +1,21 @@
 class FridgesController < ApplicationController
   def index
+    @fridge = Fridge.new
   end
+
+  def create
+    @fridge = Fridge.new(fridge_params)
+    if @fridge.save
+      user = User.find(current_user.id)
+      user.update(fridge_id: @fridge.id)
+      redirect_to root_path
+    else
+      render :index
+  end
+
+
+  private
+    def fridge_params
+      params.require(:fridge).permit(:title)
+    end
 end
