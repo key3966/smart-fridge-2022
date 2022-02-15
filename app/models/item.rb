428cdcbdfeeadbbec240831cdf13db70a9ef3dac
key3966 <ev0.kpx@gmail.com>
2022-02-15 17:18:44 +0900

    itemモデルアソシエーション・バリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,15 @@
 class Item < ApplicationRecord
+
+  belongs_to :fridge
+
+  with_options presence: true do
+    validates :regular
+    validates :name
+    validates :exp_date
+  end
+
+  with_options numericality: { other_than: 1, message: "can't be blank" } do
+    validates :category_id
+    validates :amount_id
+  end
 end
