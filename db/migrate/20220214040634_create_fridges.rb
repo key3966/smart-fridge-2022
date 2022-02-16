c4883f536d24366355451d407f0419017a215f8f
key3966 <ev0.kpx@gmail.com>
2022-02-16 18:14:17 +0900

    ClearDBでutfmb4を使う設定をマイグレーションファイルに追記

--- db/migrate/20220214040634_create_fridges.rb
+++ db/migrate/20220214040634_create_fridges.rb
@@ -1,6 +1,6 @@
 class CreateFridges < ActiveRecord::Migration[6.0]
   def change
-    create_table :fridges do |t|
+    create_table :fridges, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4' do |t|
       t.string :title
       t.timestamps
     end
--- db/migrate/20220215080343_create_items.rb
+++ db/migrate/20220215080343_create_items.rb
@@ -1,6 +1,6 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
-    create_table :items do |t|
+    create_table :items, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4' do |t|
       t.integer    :regular,     null: false
       t.string     :name,        null: false
       t.integer    :category_id, null: false
