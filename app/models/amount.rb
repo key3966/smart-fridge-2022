d08c73a9c5c67a41cd31d46d9cff99eb60af84cb
key3966 <ev0.kpx@gmail.com>
2022-02-16 15:35:37 +0900

    誤字修正

--- app/models/amount.rb
+++ app/models/amount.rb
@@ -2,9 +2,9 @@ class Amount < ActiveHash::Base
   self.data = [
     { id: 0, name: '--' },
     { id: 1, name: 'なし' },
-    { id: 3, name: '少ない' },
-    { id: 4, name: '半分程度' },
-    { id: 5, name: '多い' },
+    { id: 2, name: '少ない' },
+    { id: 3, name: '半分程度' },
+    { id: 4, name: '多い' },
   ]
 
   include ActiveHash::Associations
