e3734daa89ee1e1385c844600723bf23b9ed3025
key3966 <ev0.kpx@gmail.com>
2022-02-17 18:26:40 +0900

    shopping_itemsモデル作成

--- db/migrate/20220217091740_create_shopping_items.rb
+++ db/migrate/20220217091740_create_shopping_items.rb
@@ -1,8 +1,9 @@
 class CreateShoppingItems < ActiveRecord::Migration[6.0]
   def change
-    create_table :shopping_items do |t|
-
-      t.timestamps
+    create_table :shopping_items, options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4' do |t|
+      t.references :shopping, null: false, foreign_key: true
+      t.references :item,     null: false, foreign_key: true
+      t.timestamps 
     end
   end
 end
