ad104999aa00f3086706935b5633650e0aba743b
key3966 <ev0.kpx@gmail.com>
2022-02-17 18:10:24 +0900

    shoppingsコントローラー内アクション記述

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -1,4 +1,8 @@
 class ShoppingsController < ApplicationController
+
   def index
+    fridge = Fridge.find(params[:fridge_id])
+    @items = fridge.items.order(regular: :desc, amount_id: :asc)
   end
+
 end
