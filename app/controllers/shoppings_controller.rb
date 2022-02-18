6dee93c87655eea5b0bee92c784ac91137ca67f7
key3966 <ev0.kpx@gmail.com>
2022-02-18 11:34:50 +0900

    shoppingsコントローラーへフォームオブジェクトとストロングパラメーターを記述

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -3,6 +3,21 @@ class ShoppingsController < ApplicationController
   def index
     fridge = Fridge.find(params[:fridge_id])
     @items = fridge.items.order(regular: :desc, amount_id: :asc)
+    @shopping_form = ShoppingForm.new
   end
 
+  def create
+    @shopping_form = ShoppingForm.new(shopping_params)
+    if @shopping_form.valid?
+      @shopping_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def shopping_params
+    params.require(:shopping_form).permit(:shopping_date, :shoping_item_ids).merge(fridge_id: fridge_id, user_id: current_user.id)
 end
