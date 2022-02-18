ce08aa1a575c493652dad497667430301ff336e6
key3966 <ev0.kpx@gmail.com>
2022-02-18 17:53:15 +0900

    createアクション詳細編集

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,9 +1,22 @@
 class RequestsController < ApplicationController
   def create
-
+    @request = Request.new(request_params)
+    if @request.save
+      redirect_to root_path
+    else
+      @item = Item.new
+      @items = current_user.fridge.items
+      render template: 'fridges/index'
+    end
   end
 
   def destroy
   end
-  
+
+  private
+
+  def request_params
+    params.require(:request).permit(:fridge_id, :user_id)
+  end
+
 end
