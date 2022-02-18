76d5307fd3662909e644ae9d9cc97022ea55f485
key3966 <ev0.kpx@gmail.com>
2022-02-18 15:16:24 +0900

    ログイン状態でなけばリダイレクトの処理・リファクタリング

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -1,13 +1,13 @@
 class ShoppingsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_fridge, only: [:index, :create]
 
   def index
-    fridge = Fridge.find(params[:fridge_id])
     @items = fridge.items.order(regular: :desc, amount_id: :asc)
     @shopping_form = ShoppingForm.new
   end
 
   def create
-    fridge = Fridge.find(params[:fridge_id])
     @shopping_form = ShoppingForm.new(shopping_params)
     if @shopping_form.valid?
       @shopping_form.save
@@ -22,5 +22,9 @@ class ShoppingsController < ApplicationController
 
   def shopping_params
     params.require(:shopping_form).permit(:shopping_date, shopping_item_ids:[]).merge(fridge_id: params[:fridge_id], user_id: current_user.id)
+  end
+
+  def set_fridge
+    fridge = Fridge.find(params[:fridge_id])
   end
 end
