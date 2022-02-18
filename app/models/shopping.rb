f397e31647eb79facfe98a338ce7c7932c25eb3d
key3966 <ev0.kpx@gmail.com>
2022-02-18 12:36:06 +0900

    多対多のアソシエーション記述を修正

--- app/models/shopping.rb
+++ app/models/shopping.rb
@@ -1,6 +1,6 @@
 class Shopping < ApplicationRecord
   belongs_to :fridge
   belongs_to :user
-  has_many :items, through: :shopping_items
   has_many :shopping_items
+  has_many :items, through: :shopping_items
 end
