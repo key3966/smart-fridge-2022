51c9750064d200dc64c58076a69f21c6d043eb36
key3966 <ev0.kpx@gmail.com>
2022-02-19 10:54:58 +0900

    ルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: "fridges#index"
-  resources :fridges, only: [:index, :create, :update] do
+  resources :fridges, only: [:index, :create] do
     resources :shoppings, only: [:index, :create]
   end
   resources :items, only: [:create, :update, :destroy]
-  resources :requests, only: [:create, :destroy]
+  resources :requests, only: [:create, :update, :destroy]
 end
