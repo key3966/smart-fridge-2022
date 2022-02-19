bcdfe624b9f007d0bfce8337f646aba987ac6f8a
key3966 <ev0.kpx@gmail.com>
2022-02-19 10:55:50 +0900

    requestsコントローラーに処理を記述・リファクタリング

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,19 +1,26 @@
 class RequestsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_request, only: [:update, :destroy]
 
   def create
     @request = Request.new(request_params)
     if @request.save
       redirect_to root_path
     else
-      @item = Item.new
-      @items = current_user.fridge.items
-      render template: 'fridges/index'
+      render_top
+    end
+  end
+
+  def update
+    if @request.user.update(fridge_id: @request.fridge_id)
+      @request.destroy
+      redirect_to root_path
+    else
+      render_top
     end
   end
 
   def destroy
-    @request = Request.find(params[:id])
     @request.destroy
     redirect_to root_path
   end
@@ -24,4 +31,13 @@ class RequestsController < ApplicationController
     params.require(:request).permit(:fridge_id, :user_id)
   end
 
+  def set_request
+    @request = Request.find(params[:id])
+  end
+
+  def render_top
+    @item = Item.new
+    @items = current_user.fridge.items if user_signed_in? && current_user.fridge.present?
+    render template: 'fridges/index'
+  end 
 end
