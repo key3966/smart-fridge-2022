fccd7e0c2300dd56daf8e4c1ec1333df84408fd2
key3966 <ev0.kpx@gmail.com>
2022-03-03 17:37:10 +0900

    lossesコントローラーのcreateアクションのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   end
   resources :items, only: [:create, :update, :destroy]
   resources :requests, only: [:create, :update, :destroy]
+  resources :losses, only: :create
 end
